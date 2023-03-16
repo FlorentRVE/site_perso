b73c3a657a2781fd104b6d502ab8015b6997697e
FlorentRVE <108260642+FlorentRVE@users.noreply.github.com>
2023-03-16 23:42:51 +0100

    Revert "Carousel: Replace anchor tags by Link components to prevent page reload"

--- src/component/header_carrousel.js
+++ src/component/header_carrousel.js
@@ -1,7 +1,6 @@
 //=============================== Header ==========================
 
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { Carousel } from 'react-carousel3';
 
 
@@ -12,26 +11,26 @@ const HeaderCarrousel = () => {
         <div className="mt-[-170px] mb-12">
             <Carousel height={400} width={'100%'} yOrigin={32} yRadius={32}>
 
-                <Link to='#competences'>
+                <a href='#competences'>
                     <div key={1} className="w-44 h-44 p-2 hover:scale-110">
                         <img alt="" src="images/competences.png" className=''/>
                         <h3 className='text-center text-xl uppercase'>Competences</h3>
                     </div>
-                </Link>
+                </a>
 
-                <Link to='#portfolio'>
+                <a href='#portfolio'>
                     <div key={1} className="w-44 h-44 p-2 hover:scale-110">
                         <img alt="" src="images/portfolio.png" />
                         <h3 className='text-center text-xl uppercase'>Portfolio</h3>
                     </div>
-                </Link>
+                </a>
 
-                <Link to='#contact'>
+                <a href='#contact'>
                     <div key={1} className="w-44 h-44 p-2 hover:scale-110">
                         <img alt="" src="images/contact.png" />
                         <h3 className='text-center text-xl uppercase'>Contact</h3>
                     </div>
-                </Link>
+                </a>
 
             </Carousel>
 
